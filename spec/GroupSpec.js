fbb0f604ede9dc40fde0fd0d97622f78630f5e76
Chris Peters <cpeters@pingidentity.com>
2016-10-22 12:19:10 -0700

    begin adding nested iteration

--- spec/GroupSpec.js
+++ spec/GroupSpec.js
@@ -15,4 +15,21 @@ describe("Group", ()=> {
         expect(group.sprite instanceof Sprite).toBe(true);
         expect(group.isGroup).toBe(true);
     });
+
+    it("iterates over other groups", ()=> {
+        let item = { uuid: 4 };
+        let group2 = new Group();
+        let itemFound = false;
+
+        group2.collection.add(item);
+        group.collection.add(group2);
+
+        group.collection.each( item => {
+            if (item.uuid === 4) {
+                itemFound = true;
+            }
+        });
+
+        expect(itemFound).toBe(true);
+    });
 });
